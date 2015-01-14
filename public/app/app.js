c4f59ef19ee05c48eefabcf83c8f67942bd5c39b
noah <noahgray@me.com>
2015-01-14 08:21:24 -0800

    login form

--- public/app/app.js
+++ public/app/app.js
@@ -9,8 +9,34 @@ var app = angular.module('SlipStream', ['ui.router'])
 			templateUrl: 'views/home.html',
 			controller: 'HomeController'
 		})
+		.state( '/login', {
+			url: '/login',
+			templateUrl: 'views/login.html',
+			controller: 'LoginController'
+		})
 }])
 
 .controller('HomeController', ['$scope', '$state', '$urlRouter', '$http', function( $scope, $state, $urlRouter, $http ) {
 	$scope.message = "hello"
+}])
+
+.controller('LoginController', ['$scope', '$state', '$urlRouter', '$http', function( $scope, $state, $urlRouter, $http ) {
+	$scope.user = {
+		username: '',
+		password: ''
+	}
+	
+	$scope.login = function() {
+		console.log( "Username: " + $scope.user.username + " Password: " + $scope.user.password )
+		$http
+			.post( '/api/authenticate', $scope.user )
+			.success( function ( data, status ) {
+				console.log( "Authentication successful: " + data.token )
+				window.sessionStorage.token = data.token
+			} )
+			.error( function ( data, status ) {
+				delete $window.sessionStorage.token
+				alert( "Error signing in: " + status )
+			} )
+	}
 }])
