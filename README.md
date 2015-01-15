deb9965c329dd74b5b30f2e7ef7d8cd4ca6e9680
noah <noahgray@me.com>
2015-01-15 09:44:22 -0800

    login page, app page, logout, styling

--- README.md
+++ README.md
@@ -1,12 +1,24 @@
 # SlipStream
 
-SlipStream is a content-based social network. 
+SlipStream is a content-based social network.
 
 # Installtion
 
-The app uses Bower and NPM for dependencies. To install, download and install node from http://nodejs.org. Then do,
+The app uses *Bower* and *NPM* for dependencies. To install, download and install *Node* from http://nodejs.org. Then do,
 
 	cd slipstream
 	npm install -g bower
 	npm install
 	bower install
+
+It also uses *MongoDB* which requires *Homebrew* to install on Mac. See http://brew.sh. Then:
+
+	brew install mongodb
+
+# Run
+
+To run the server, simple do,
+
+	node server.js
+
+Now visit http://localhost:4000/ in your browser and you should see the main landing page.
--- public/app/app.js
+++ public/app/app.js
@@ -1,12 +1,16 @@
-var app = angular.module('SlipStream', ['ui.router'])
+var app = angular.module('SlipStream', ['ui.router', 'ngStorage'])
 
 .config( [ '$stateProvider', '$urlRouterProvider', '$httpProvider', function( $stateProvider, $urlRouterProvider, $httpProvider ) {
 	$urlRouterProvider.otherwise('/home')
 
+	//
+	// add the custom service to add Authenticaiotn to header
+	//
 	$httpProvider.interceptors.push('authInterceptor')
+	// $httpProvider.interceptors.push('authCheck')
 
 	$stateProvider
-		.state( '/home', {
+		.state( 'home', {
 			url: '/home',
 			templateUrl: 'views/home.html',
 			controller: 'HomeController'
@@ -23,8 +27,53 @@ var app = angular.module('SlipStream', ['ui.router'])
 		})
 }])
 
-.controller('HomeController', ['$scope', '$state', '$urlRouter', '$http', function( $scope, $state, $urlRouter, $http ) {
-	$scope.message = "hello"
+.controller('HomeController', ['$scope', '$state', '$urlRouter', '$http', '$window', '$sessionStorage', '$location', function( $scope, $state, $urlRouter, $http, $window, $sessionStorage, $location ) {
+	$scope.user = {
+		username: '',
+		password: ''
+	}
+
+	$scope.login = function() {
+		$http
+			.post( '/api/authenticate', $scope.user )
+			.success( function ( data, status ) {
+				$window.sessionStorage.token = data.token
+				location.reload()
+			} )
+			.error( function ( data, status ) {
+				delete $window.sessionStorage.token
+				console.log( "Error signing in: " + status )
+			} )
+	}
+
+	$scope.logout = function() {
+			delete $window.sessionStorage.token
+			location.reload()
+	}
+
+	//
+	// check if there is sessionStorage, which is probably an auth token
+	//
+	var init = function () {
+		if ( $window.sessionStorage.length > 0 )
+			$scope.isLoggedIn = true
+	}
+
+	//
+	// deletes the current account
+	//
+	$scope.deleteAccount = function () {
+		$http
+			.delete( '/api/users' )
+			.success( function () {
+				delete $window.sessionStorage.token
+				$state.go( 'home' )
+				location.reload()
+			})
+	}
+
+	init()
+
 }])
 
 .controller('ProfileController', ['$scope', '$state', '$urlRouter', '$http', function( $scope, $state, $urlRouter, $http ) {
@@ -33,32 +82,17 @@ var app = angular.module('SlipStream', ['ui.router'])
 		.get( '/api/users' )
 			.success( function( data ) {
 				$scope.user = data
-				console.log( $scope.user.email )
 			})
 			.error( function( error ) {
 				console.log( "Error retrieving user: " + error )
 			})
+
 }])
 
 .controller('LoginController', ['$scope', '$window', '$state', '$urlRouter', '$http', function( $scope, $window, $state, $urlRouter, $http ) {
-	$scope.user = {
-		username: '',
-		password: ''
-	}
 
-	$scope.login = function() {
-		console.log( "Username: " + $scope.user.username + " Password: " + $scope.user.password )
-		$http
-			.post( '/api/authenticate', $scope.user )
-				.success( function ( data, status ) {
-					console.log( "Authentication successful: " + data.token )
-					$window.sessionStorage.token = data.token
-				} )
-				.error( function ( data, status ) {
-					delete window.sessionStorage.token
-					console.log( "Error signing in: " + status )
-				} )
-	}
+
+
 }])
 
 //
@@ -72,10 +106,19 @@ var app = angular.module('SlipStream', ['ui.router'])
 				config.headers.Authorization = 'Bearer ' + $window.sessionStorage.token
 			}
 			return config
-		},
-//		response: function ( response ) {
-//			if ( response.status === 403 )
-//				console.log ( "Authorization failed" )
-//			}
+		}
 	}
 } ] )
+
+	//
+	// set a scope variable for whether a user is signed in or not
+	//
+	// .factory('authCheck', [ '$window', '$rootScope', function ( $window, $rootScope ) {
+	// 		$rootScope.isLoggedIn = false;
+	//
+	// 		if ( $window.sessionStorage.token ) {
+	// 			$rootScope.isLoggedIn = true;
+	// 		}
+	//
+	// 		return $rootScope.isLoggedIn;
+	// }])
