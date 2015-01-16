d2752339fcb07881c9a9a548de2a24350080436f
noah <noahgray@me.com>
2015-01-16 15:36:51 -0800

    front end API call works

--- public/app/app.js
+++ public/app/app.js
@@ -123,7 +123,14 @@ var app = angular.module('SlipStream', ['ui.router', 'ui.bootstrap'])
 
 .controller('ArticlesController', ['$scope', '$window', '$state', '$urlRouter', '$http', function( $scope, $window, $state, $urlRouter, $http ) {
 
-
+	$http
+		.get( 'api/stream/articles')
+			.success( function ( data ) {
+				$scope.articles = data
+			})
+			.error( function ( error ) {
+				console.log( 'Error: ' + error)
+			})
 
 
 }])
