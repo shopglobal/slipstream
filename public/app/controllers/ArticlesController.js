33a708f8f76afcc325ccd1a8a1720e7a5b8e0ea0
noah <noahgray@me.com>
2015-02-11 12:54:09 -0800

    Infinite scrolling - Watch (front-end)
    [ Delivers #87901892 ] Adds endless scrolling to front-end for Watch
    stream

--- public/app/controllers/ArticlesController.js
+++ public/app/controllers/ArticlesController.js
@@ -1,34 +1,5 @@
 app.controller('ArticlesController', ['$scope', '$window', '$state', '$urlRouter', '$http', 'Content', function( $scope, $window, $state, $urlRouter, $http, Content ) {
 
-	// $http
-	// 	.get( 'api/stream/read', { params: { 
-	// 		show: 5, 
-	// 		page: 1 
-	// 	} } )
-	// 		.success( function ( data ) {
-	// 			$scope.articles = data
-	// 		})
-	// 		.error( function ( error ) {
-	// 			console.log( 'Error: ' + error)
-	// 		})
-
 	$scope.articles = new Content()
-
-	// $scope.loadMore = function () {
-		
-	// 	var last = ( $scope.articles.length / 3 )
-
-	// 	$http
-	// 		.get( 'api/stream/read', { params: { 
-	// 			show: 3, 
-	// 			page: last + 1
-	// 		} } )
-	// 			.success( function ( data ) {
-	// 				$scope.articles.push( data )
-	// 			})
-	// 			.error( function ( error ) {
-	// 				console.log( 'Error: ' + error)
-	// 			})
-	// }
 
 }])
