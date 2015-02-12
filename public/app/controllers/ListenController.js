12814361ffdb50552cbab7b743dbbe751a1e9b0c
noah <noahgray@me.com>
2015-02-11 18:56:34 -0800

    Infinite scrolling Listen (front-end)
    [ Delivers #88185356 ] Adds endless scrolling to listen stream

--- public/app/controllers/ListenController.js
+++ public/app/controllers/ListenController.js
@@ -1,16 +1,9 @@
-app.controller('ListenController', ['$scope', '$window', '$state', '$urlRouter', '$http', function( $scope, $window, $state, $urlRouter, $http ) {
+app.controller('ListenController', ['$scope', '$window', '$state', '$urlRouter', '$http', 'Content', function( $scope, $window, $state, $urlRouter, $http, Content ) {
 
 	$scope.embedUrl = function ( videoId ) {
 		return "https://w.soundcloud.com/player/?url=https://api.soundcloud.com/tracks/" + videoId
 	}
 
-	$http
-		.get( 'api/stream/listen')
-			.success( function ( data ) {
-				$scope.songs = data
-			})
-			.error( function ( error ) {
-				console.log( 'Error: ' + error)
-			})
+	$scope.songs = new Content()
 
 }])
