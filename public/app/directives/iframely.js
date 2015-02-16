b3bce7501bd9e8047c0268286015e6fab5ba1185
noah <noahgray@me.com>
2015-02-16 11:02:19 -0800

    New directive for getting any embed
    [ #87896726 ] Adds a directive to get any embed using the iFramely
    service. This service must be installed separately on the VPN before it
    will work.

---
+++ public/app/directives/iframely.js
@@ -0,0 +1,25 @@
+
+	angular.module( 'iframely', [])
+
+	.directive( 'iframely', [ '$http', '$sce', function ( $http, $sce ) {
+		return {
+			replace: true,
+			restrict: "AE",
+			scope: {
+				url: '@'
+			},
+			template: '<div ng-bind-html="content"></div>',
+			link: function ( scope, element, attrs ) {
+				$http( {
+					url: 'http://localhost:8061/iframely',
+					method: 'GET',
+					params: {
+						url: attrs.url
+					}
+				})
+				.then( function ( result ) {
+					scope.content = $sce.trustAsHtml( result.data.html )
+				})
+			}
+		}
+	}])
