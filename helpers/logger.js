99faee7ec75a044edfb7d36ed7d4639fc0c166d6
noah <noahgray@me.com>
2015-02-26 21:57:59 -0800

    log path bug

--- helpers/logger.js
+++ helpers/logger.js
@@ -3,18 +3,18 @@
 This just sets the default logger. 
 
 */
-//var path = require( 'path' )
+var path = require( 'path' )
 
 module.exports = require( 'bunyan' ).createLogger( {
 	name: "SlipStream",
 	streams: [ { 
 		level: 'error',
-		path: '../logs/error.log',
+		path: path.join( __dirname, '../logs/info.log'),
 		type: 'rotating-file'
 	},
 	{
 		level: 'info',
-		path: '../logs/info.log',
+		path: path.join( __dirname, '../logs/info.log'),
 		type: 'rotating-file' 
 	}]
 })
