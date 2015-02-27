d2c5da0b1d04359f3f08964a28914ea72f4a3aa8
noah <noahgray@me.com>
2015-02-26 21:44:47 -0800

    error log path bug
    fixes bug that occurs on remote servers.

--- helpers/logger.js
+++ helpers/logger.js
@@ -3,16 +3,18 @@
 This just sets the default logger. 
 
 */
+//var path = require( 'path' )
+
 module.exports = require( 'bunyan' ).createLogger( {
 	name: "SlipStream",
 	streams: [ { 
 		level: 'error',
-		path: './logs/error.log',
+		path: __dirname + '/logs/error.log',
 		type: 'rotating-file'
 	},
 	{
 		level: 'info',
-		path: './logs/info.log',
+		path: __dirname + '/logs/info.log',
 		type: 'rotating-file' 
 	}]
 })
