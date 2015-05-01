69d792c466dbea81376fca0ce1cf699c61285696
OKNoah <noahgray@me.com>
2015-05-01 00:46:27 -0700

    Removes space in SlipStream emails
    [ Delivers #93289286 ] Removes space between “slip stream” in emails.

--- controllers/userController.js
+++ controllers/userController.js
@@ -269,7 +269,7 @@ exports.sendPasswordReset = function ( req, res ) {
 				from: 'SlipStream <noahgray@me.com>',
 				to: user.email,
 				subject: 'Your temporary SlipStream password',
-				html: passwordHtml.toString() + user.temporaryPassword + "</code></div><br /><a href='http://beta.slipstreamapp.com'><div class='button-visit'>Take me to slip stream.</div></a></div></html>"
+				html: passwordHtml.toString() + user.temporaryPassword + "</code></div><br /><a href='http://beta.slipstreamapp.com'><div class='button-visit'>Take me to SlipStream.</div></a></div></html>"
 			}
 
 			mailgun.messages().send( email, function ( err, body ) {
