a96109c5d8e6c52bfad3e471156816dd1343a9a2
OKNoah <OKNoah@users.noreply.github.com>
2015-05-25 14:41:50 -0700

    Cleans up some server code
    Cleans up some leftover code in the main server files

--- routes/home.js
+++ routes/home.js
@@ -1,26 +1,15 @@
 var express = require('express'),
 	router = express.Router(),
 	path = require('path'),
-	fs = require( 'fs' ),
-	tls = require( 'tls' )
+	fs = require( 'fs' )
 
 var indexPath = path.join( __dirname, '..public/index.html')
-
-var options = {
-	key: fs.readFileSync( 'server.key'),
-	cert: fs.readFileSync( 'server.crt' )
-}
 
 // 
 // serves he index file when accessing root URL
 // 
 router.get('/', function( rez, res, next ) {
 	res.writeHead( 200, {'Content-Type': 'text/html'} )
-	
-	tls.createServer( function ( s ) {
-		s.write( 'public/index.html' )
-		s.pipe( s )
-	} ).listen( 4000 )
 	
 	fs.createReadStream('public/index.html')
 		.on('open', function () {
